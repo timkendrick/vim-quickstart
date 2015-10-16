80caa860b6b7efa70fe9b1bcc30419035610e7e1
Tim Kendrick <timkendrick@gmail.com>
2015-10-16 02:41:31 +0100

    Add progress message to install script

--- install/macosx.sh
+++ install/macosx.sh
@@ -2,6 +2,7 @@
 set -e
 cd ~
 
+echo "[vim-quickstart] Downloading latest version of vim-quickstart..."
 if [ ! -d vim-quickstart ]; then
 	git clone https://github.com/timkendrick/vim-quickstart.git
 	echo "source $(pwd)/vim-quickstart/quickstart.vim" >> ~/.vimrc
@@ -11,14 +12,20 @@ else
 	cd ..
 fi
 
+echo "[vim-quickstart] Installing Vundle..."
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
+echo "[vim-quickstart] Installing Vundle plugins..."
 vim -c PluginInstall -c qa
 
+echo "[vim-quickstart] Installing additional binaries..."
 brew install caskroom/cask/brew-cask editorconfig the_silver_searcher macvim
 brew tap caskroom/fonts
 brew cask install font-dejavu-sans-mono-for-powerline
 
 ~/.vim/bundle/YouCompleteMe/install.py
+
+echo ""
+echo "[vim-quickstart] Installed successfully!"
